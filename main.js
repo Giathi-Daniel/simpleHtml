d80250c828aa8f0012cf7ddacee72c47a65034cb
Daniel Giathi <giathidaniel252@gmail.com>
2024-02-02 09:56:06 +0300

    completed

--- main.js
+++ main.js
@@ -2,9 +2,22 @@ $(document).ready(function() {
 
     $('#login-btn').click(function() {
         $('.login-form').toggleClass('active');
+        $('#menu-btn').removeClass('fa-times');
+        $('.navbar').removeClass('active');
     });
 
+    $('#menu-btn').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('active');
+        $('.login-form').removeClass('active');
+    })
+
     $(window).on('scroll load', function() {
+
+        $('#menu-btn').removeClass('fa-times');
+        $('.navbar').removeClass('active');
+        $('.login-form').removeClass('active');
+
         $('section').each(function() {
             let top = $(window).scrollTop();
             let height = $(this).height();

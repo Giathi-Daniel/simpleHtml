b5c58387129726fed7613933adf9d88fdbb12d82
Daniel Giathi <giathidaniel252@gmail.com>
2024-02-02 09:23:06 +0300

    barafu resort

--- main.js
+++ main.js
@@ -3,4 +3,18 @@ $(document).ready(function() {
     $('#login-btn').click(function() {
         $('.login-form').toggleClass('active');
     });
+
+    $(window).on('scroll load', function() {
+        $('section').each(function() {
+            let top = $(window).scrollTop();
+            let height = $(this).height();
+            let offset = $(this).offset().top - 200;
+            let id = $(this).attr('id');
+    
+            if(top >= offset && top < offset + height) {
+                $('.navbar a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+        });
+    })
 });
